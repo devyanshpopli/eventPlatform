31cadd62af843b72f31ec4977b3af88f2b29f6b1
devyanshpopli <devyanshpopli40@gmail.com>
2024-01-22 00:11:35 +0530

    first Commit

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,14 @@
+import { Schema,model,models } from "mongoose"
+
+const userSchema = new Schema({
+    clerkId:{type:String, required:true, unique:true},
+    email:{type:String, required:true, unique:true},
+    username:{type:String, required:true, unique:true},
+    firstName:{type:String, required:true, unique:true},
+    lastName:{type:String, required:true, unique:true},
+    photo:{type:String, unique:true}
+})
+
+const User = models.User || model('User',userSchema);
+
+export default User;
